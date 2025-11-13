0a0aaf7eba89f541d82c2c058701a333a10e1fb5
Hugo Olivera Alonso <hugo.olivera@gmail.com>
2025-11-13 20:50:21 -0300

    Add project files.

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Producto>> AddProducto(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProductos), new { id = producto.Id }, producto);
+        }
+    }
+}
